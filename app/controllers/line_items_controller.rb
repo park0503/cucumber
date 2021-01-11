0f1e14827ce8dd58f984918ead140df8c097e4a3
park0503 <hooo0503@gmail.com>
2021-01-11 18:11:39 +0900

    implement buy button in cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,7 +1,7 @@
 class LineItemsController < ApplicationController
     def index
-        order = Order.find_by(user_id: current_user.id, status: 0)
-        @line_items = LineItem.where(order_id: order.id)
+        @order = Order.find_by(user_id: current_user.id, status: 0)
+        @line_items = LineItem.where(order_id: @order.id)
     end
 
     def create
@@ -11,14 +11,24 @@ class LineItemsController < ApplicationController
         line_item.item_id = item.id
         line_item.order_id = order.id
         line_item.save
+        order.update(amount: order.amount + line_item.amount)
         redirect_to item_path(params[:id])
     end
 
     def destroy
         order = Order.find_by(user_id: current_user.id, status: 0)
         line_item = LineItem.find_by(order_id: order.id, item_id: params[:id])
+        order.update(amount: order.amount - line_item.amount)
         line_item.destroy
         redirect_to item_path(params[:id])
+    end
+
+    def destroy_in_cart
+        line_item = LineItem.find(params[:id])
+        order = Order.find_by(user_id: current_user.id, status: 0)
+        order.update(amount: order.amount - line_item.amount)
+        line_item.destroy
+        redirect_to line_items_path
     end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,16 @@ Rails.application.routes.draw do
   resources :items
   root "items#index"
   get "users/mypage"
-  resources :users
-  resources :line_items
+  resources :users do
+    collection do
+      get :mypage
+    end
+  end
+  resources :line_items do
+    member do
+      delete :destroy_in_cart
+    end
+  end
+  resources :orders
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
